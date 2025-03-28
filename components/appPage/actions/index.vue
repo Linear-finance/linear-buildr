1330fc96d6483ca2b11a3962132206009aed0128
mikkel-linear <152798779+mikkel-linear@users.noreply.github.com>
2025-03-28 16:28:54 +0100

    chore: update menu (#40)

--- components/appPage/actions/index.vue
+++ components/appPage/actions/index.vue
@@ -295,12 +295,8 @@ export default {
         { name: "Builder", url: "https://builder.linear.finance/" },
         { name: "Bridge", url: "https://builder.linear.finance/bridge" },
         { name: "Exchange", url: "https://exchange.linear.finance/" },
-        { name: "Dashboard", url: "https://dashboard.linear.finance/" },
-        // { name: "Vault", url: "https://vault.linear.finance/" },
         { name: "Liquidator", url: "https://liquidator.linear.finance/" },
-        { name: "Marketplace", url: "https://marketplace.linear.finance/" },
         { name: "PerpDEX", url: "https://perpdex.linear.finance/" },
-        { name: "DAO", url: "https://forum.linear.finance/" },
       ],
     };
   },
